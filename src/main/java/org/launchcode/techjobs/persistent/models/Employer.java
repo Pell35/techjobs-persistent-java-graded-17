9f288a45ef07c94755fcb8870a0ace4304df59c6
Pell35 <pjelliott11@gmail.com>
2023-11-12 21:07:44 -0600

    Task 3 Completed, tests for this task and previous are passing.

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,36 @@
 package org.launchcode.techjobs.persistent.models;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+   @OneToMany
+   @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
+    @NotNull
+    @Size(min = 3, max = 255)
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    //    Do I need a getLocation or getEmployer?
+    public Employer(){
+        //my no-arg constructor, change name?
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,21 @@
 package org.launchcode.techjobs.persistent.models;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
 
+@Entity
 public class Skill extends AbstractEntity {
+    private String description;
 
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Skill() {
+        //no-arg, different name?
+    }
 }
